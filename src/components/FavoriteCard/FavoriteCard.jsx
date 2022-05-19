e7831f84c1ab02b018a2459d77203909f02c98f7
GergelyKiss <gergely.kiss.hun@gmail.com>
2022-05-19 22:50:46 +0200

    styling

--- src/components/FavoriteCard/FavoriteCard.jsx
+++ src/components/FavoriteCard/FavoriteCard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './FavoriteCard.css';
 import { useDispatch } from 'react-redux'
 import FavoriteIcon from '@mui/icons-material/Favorite';
@@ -8,22 +8,44 @@ import { removeFavorite } from '../../store/favorites';
 
 const FavoriteCard = ({info, image}) => {
   const dispatch = useDispatch();
-
   const {title, id, department_title, artist_display} = info;
   
+  const [isRemoving, setIsRemoving] = useState(false);
+
+
   const removeFromFavorites = () => {
-    // are you sure?
-    alert('are you sure?');
     dispatch(removeFavorite(id));
+    toggleRemove();
+  };
+
+  const toggleRemove = () => {
+    setIsRemoving(prev => !prev);
   };
 
   return (
     <div className='favorite-card'>
-      <h2>{title}</h2>
-      <h3>{artist_display}</h3>
-      <p>{department_title}</p>
-      <img src={image} alt="artwork" />
-      <FavoriteIcon onClick={removeFromFavorites} className='favorite-heart-icon'/>
+      <img className='favorite-img' src={image} alt="artwork" />
+      <div className='favorite-info'>
+        {isRemoving ?
+          <>
+            <h3 className='remove-question'>Remove from favorites?</h3>
+            <div className='remove-choices-container'>
+              <button className="primary-btn" onClick={removeFromFavorites}>Confirm</button>
+              <button className="secondary-btn" onClick={toggleRemove}>Back</button>
+            </div>
+          </> 
+          :
+          <>
+            <h2>{title}</h2>
+            <h3>{artist_display}</h3>
+            <p>{department_title}</p>
+            <div onClick={toggleRemove} className="rm-fav-container">
+              <FavoriteIcon className='favorite-heart-icon'/>
+              <span>Remove from favorites</span>
+            </div>
+          </>
+        }
+      </div>
     </div>
   )
 }
